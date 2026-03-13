e1d5d471dfc921b461d992dcaa35fda193701a34
gobimans <gravyzappa@gmail.com>
2026-03-13 15:15:57 +0100

    Update MainActivity.kt

--- app/src/main/java/com/gobimans/cam/MainActivity.kt
+++ app/src/main/java/com/gobimans/cam/MainActivity.kt
@@ -8,10 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontSize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
